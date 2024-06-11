4b63c92fa02e507bf36abd894c130f1caf68b787
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-06-11 16:44:51 -0600

    changing the schema model

--- prisma/migrations/20240611205521_/migration.sql
+++ prisma/migrations/20240611205521_/migration.sql
@@ -4,8 +4,6 @@ CREATE TABLE `CompanyDetail` (
     `section_id` INTEGER NULL,
     `name` VARCHAR(191) NOT NULL,
 
-
-    UNIQUE INDEX `CompanyDetail_section_id_key`(`section_id`),
     INDEX `section_id`(`section_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -18,8 +16,6 @@ CREATE TABLE `CompanyDetailsContent` (
     `content` VARCHAR(191) NULL,
     `url` VARCHAR(191) NULL,
 
-
-    UNIQUE INDEX `CompanyDetailsContent_company_details_id_key`(`company_details_id`),
     INDEX `company_details_id`(`company_details_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -30,8 +26,6 @@ CREATE TABLE `ContactInformation` (
     `section_id` INTEGER NULL,
     `name` VARCHAR(191) NOT NULL,
 
-
-    UNIQUE INDEX `ContactInformation_section_id_key`(`section_id`),
     INDEX `section_id`(`section_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -44,8 +38,6 @@ CREATE TABLE `ContactInformationContent` (
     `content` VARCHAR(191) NULL,
     `url` VARCHAR(191) NULL,
 
-
-    UNIQUE INDEX `ContactInformationContent_contact_information_id_key`(`contact_information_id`),
     INDEX `contact_information_id`(`contact_information_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -54,7 +46,6 @@ CREATE TABLE `ContactInformationContent` (
 CREATE TABLE `Page` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
-
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -67,8 +58,6 @@ CREATE TABLE `ReferenceContent` (
     `content` VARCHAR(191) NULL,
     `url` VARCHAR(191) NULL,
 
-
-    UNIQUE INDEX `ReferenceContent_reference_id_key`(`reference_id`),
     INDEX `reference_id`(`reference_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -82,8 +71,6 @@ CREATE TABLE `SectionContent` (
     `url` VARCHAR(191) NULL,
     `link_text` VARCHAR(191) NULL,
 
-
-    UNIQUE INDEX `SectionContent_section_id_key`(`section_id`),
     INDEX `section_id`(`section_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -94,8 +81,6 @@ CREATE TABLE `SectionReference` (
     `section_id` INTEGER NULL,
     `name` VARCHAR(191) NOT NULL,
 
-
-    UNIQUE INDEX `SectionReference_section_id_key`(`section_id`),
     INDEX `section_id`(`section_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -106,8 +91,6 @@ CREATE TABLE `Section` (
     `name` VARCHAR(191) NOT NULL,
     `page_id` INTEGER NULL,
 
-
-    UNIQUE INDEX `Section_page_id_key`(`page_id`),
     INDEX `page_id`(`page_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -121,8 +104,6 @@ CREATE TABLE `ServiceContent` (
     `url` VARCHAR(191) NULL,
     `link_text` VARCHAR(191) NULL,
 
-
-    UNIQUE INDEX `ServiceContent_service_id_key`(`service_id`),
     INDEX `service_id`(`service_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -133,8 +114,6 @@ CREATE TABLE `Service` (
     `section_id` INTEGER NULL,
     `name` VARCHAR(191) NOT NULL,
 
-
-    UNIQUE INDEX `Service_section_id_key`(`section_id`),
     INDEX `section_id`(`section_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -146,8 +125,6 @@ CREATE TABLE `SubscriptionContent` (
     `type` ENUM('title', 'subtitle', 'paragraph', 'input_text') NOT NULL,
     `content` VARCHAR(191) NULL,
 
-
-    UNIQUE INDEX `SubscriptionContent_subscription_id_key`(`subscription_id`),
     INDEX `subscription_id`(`subscription_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -158,56 +135,42 @@ CREATE TABLE `Subscription` (
     `section_id` INTEGER NULL,
     `name` VARCHAR(191) NOT NULL,
 
-
-    UNIQUE INDEX `Subscription_section_id_key`(`section_id`),
     INDEX `section_id`(`section_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `CompanyDetail`
-ADD CONSTRAINT `CompanyDetails_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `CompanyDetail` ADD CONSTRAINT `CompanyDetail_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `CompanyDetailsContent`
-ADD CONSTRAINT `CompanyDetailsContent_company_details_id_fkey` FOREIGN KEY (`company_details_id`) REFERENCES `CompanyDetail` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `CompanyDetailsContent` ADD CONSTRAINT `CompanyDetailsContent_company_details_id_fkey` FOREIGN KEY (`company_details_id`) REFERENCES `CompanyDetail`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ContactInformation`
-ADD CONSTRAINT `ContactInformation_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `ContactInformation` ADD CONSTRAINT `ContactInformation_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ContactInformationContent`
-ADD CONSTRAINT `ContactInformationContent_contact_information_id_fkey` FOREIGN KEY (`contact_information_id`) REFERENCES `ContactInformation` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `ContactInformationContent` ADD CONSTRAINT `ContactInformationContent_contact_information_id_fkey` FOREIGN KEY (`contact_information_id`) REFERENCES `ContactInformation`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ReferenceContent`
-ADD CONSTRAINT `ReferenceContent_reference_id_fkey` FOREIGN KEY (`reference_id`) REFERENCES `SectionReference` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `ReferenceContent` ADD CONSTRAINT `ReferenceContent_reference_id_fkey` FOREIGN KEY (`reference_id`) REFERENCES `SectionReference`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `SectionContent`
-ADD CONSTRAINT `SectionContent_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `SectionContent` ADD CONSTRAINT `SectionContent_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `SectionReference`
-ADD CONSTRAINT `SectionReference_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `SectionReference` ADD CONSTRAINT `SectionReference_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Section`
-ADD CONSTRAINT `Section_page_id_fkey` FOREIGN KEY (`page_id`) REFERENCES `Page` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Section` ADD CONSTRAINT `Section_page_id_fkey` FOREIGN KEY (`page_id`) REFERENCES `Page`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ServiceContent`
-ADD CONSTRAINT `ServiceContent_service_id_fkey` FOREIGN KEY (`service_id`) REFERENCES `Service` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `ServiceContent` ADD CONSTRAINT `ServiceContent_service_id_fkey` FOREIGN KEY (`service_id`) REFERENCES `Service`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Service`
-ADD CONSTRAINT `Service_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Service` ADD CONSTRAINT `Service_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `SubscriptionContent`
-ADD CONSTRAINT `SubscriptionContent_subscription_id_fkey` FOREIGN KEY (`subscription_id`) REFERENCES `Subscription` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `SubscriptionContent` ADD CONSTRAINT `SubscriptionContent_subscription_id_fkey` FOREIGN KEY (`subscription_id`) REFERENCES `Subscription`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Subscription`
-ADD CONSTRAINT `Subscription_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Subscription` ADD CONSTRAINT `Subscription_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
